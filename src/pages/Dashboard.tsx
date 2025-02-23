d82f1cbb7674405dba498eeceb96cad6ea30c8df
Saud Kadiri <saudkadiri@gmail.com>
2025-02-23 23:42:04 +0530

    added search

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,25 @@
+import { useState } from 'react';
 import StockChart from '../components/charts/StockChart';
 
 const Dashboard = () => {
+  const [search, setSearch] = useState("");
+
   return <main className="p-10 w-full backround-black rounded-xl shadow-lg background-blue-500">
     <header className="flex justify-between w-full">
       <p className="text-xl">My Dashboard</p>
-      <input type="text" name="search" id="" />
+      <section>
+        <input
+            type="text"
+            name="search"
+            placeholder="Search..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
+            Search
+          </button>
+      </section>
     </header>
     <StockChart />
   </main>

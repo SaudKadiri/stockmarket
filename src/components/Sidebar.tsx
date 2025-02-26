abeba698b3d3020af6d8d03857d73a55a529a5e8
Saud Kadiri <saudkadiri@gmail.com>
2025-02-26 22:58:08 +0530

    correct the types

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,14 +1,15 @@
 import sections from '../assets/sections'
 
+
 const Sidebar = () => {
   return (
     <main className='flex flex-col items-center justify-center rounded-r-xl shadow-lg p-4'>
       <img src="" alt="" className='h-36' />
       <div className="">
-        {Object.entries(sections).map(([key, submenus], index) => <section key={index}> 
+        {Object.entries(sections).map(([key, submenus]: [string, any], index) => <section key={index}> 
           <p className="text-xl font-bold mt-4">{key}</p>
           <ul className="">
-            {submenus.map((submenu, index) => <li key={index} className="flex items-center gap-2">
+            {submenus.map((submenu: any, index: number) => <li key={index} className="flex items-center gap-2">
               {/* <span className="material-icons">{submenu.icon}</span> */}
               <span>{submenu.title}</span>
             </li>)}

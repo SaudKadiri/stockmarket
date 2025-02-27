c1cd73b245e731a11418a361a07b651457426c9a
Saud Kadiri <saudkadiri@gmail.com>
2025-02-27 23:55:30 +0530

    Not any

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,12 +1,20 @@
 import sections from '../assets/sections'
 
+type Submenu = {
+  title: string;
+  link: string;
+};
+
+type Sections = {
+  [key: string]: Submenu[];
+};
 
 const Sidebar = () => {
   return (
     <main className='flex flex-col items-center justify-center rounded-r-xl shadow-lg p-4'>
       <img src="" alt="" className='h-36' />
       <div className="">
-        {Object.entries(sections).map(([key, submenus]: [string, any], index) => <section key={index}> 
+        {Object.entries(sections as Sections).map(([key, submenus]: [string, any], index) => <section key={index}> 
           <p className="text-xl font-bold mt-4">{key}</p>
           <ul className="">
             {submenus.map((submenu: any, index: number) => <li key={index} className="flex items-center gap-2">

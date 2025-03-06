f8a4222107669f8894aa72d5938f348543aff84a
Saud Kadiri <saudkadiri@gmail.com>
2025-03-06 23:58:02 +0530

    simulate different datasets

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -12,7 +12,6 @@ type Sections = {
 const Sidebar = () => {
   return (
     <main className='flex flex-col items-center bg-white w-80 justify-center  rounded-r-3xl shadow-lg p-4'>
-      {/* <img src="" alt="" className='h-36' /> */}
       <div className="">
         {Object.entries(sections as Sections).map(([key, submenus]: [string, any], index) => <section key={index}> 
           <p className="text-xl font-bold mt-4">{key}</p>
--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 import StockChart from './charts/StockChart'
 
 let stocks = ["AAPL", "GOOGL", "AMZN", "MSFT", "TSLA", "FB", "NVDA", "PYPL", "ADBE", "NFLX"]
-const data = {
+const dataAAPL = {
+  labels: ["Mon", "Tue", "Wed", "Thu", "Fri"], // X-axis (time)
+  datasets: [
+    {
+      label: "Stock Price ($)",
+      data: [150, 155, 160, 158, 162], // Stock price data
+      fill: false,
+      backgroundColor: "rgba(75, 192, 192, 0.2)",
+      borderColor: "rgba(0, 0, 0, 1)",
+      borderWidth: 0,
+      tension: 0.3, // Smooth curve
+    },
+  ],
+};
+
+const dataGOOGL = {
   labels: ["Mon", "Tue", "Wed", "Thu", "Fri"], // X-axis (time)
   datasets: [
     {
